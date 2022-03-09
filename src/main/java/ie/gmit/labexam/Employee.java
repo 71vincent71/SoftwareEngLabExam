b3764b327afa0c4894f33bf35c71ac7c01c224e9
Vincent <vincent71hornak@gmail.com>
2022-03-09 09:44:26 +0000

    Title Feature with Test for Valid and Invalid Title

--- src/main/java/ie/gmit/labexam/Employee.java
+++ src/main/java/ie/gmit/labexam/Employee.java
@@ -1,5 +1,7 @@
 package ie.gmit.labexam;
 
+import java.util.TimerTask;
+
 public class Employee
 {
     private String Title;
@@ -11,6 +13,22 @@ public class Employee
 
     public Employee(String Title, String Name, String PPS, String Phone, String EmploymentType, Integer Age)
     {
-
+        setTitle(Title);
     }
+    private void setTitle(String Title)
+    {
+        if(Title == "Mr" || Title == "Mrs" || Title == "Ms")
+        {
+            this.Title = Title;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Invalid Title");
+        }
+    }
+    public String getTitle()
+    {
+        return Title;
+    }
+
 }
