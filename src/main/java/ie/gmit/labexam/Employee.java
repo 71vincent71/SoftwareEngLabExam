83e5e4d3af8118f58330edd433027aa41eb59c34
Vincent <vincent71hornak@gmail.com>
2022-03-09 09:11:15 +0000

    Initial Commit

---
+++ src/main/java/ie/gmit/labexam/Employee.java
@@ -0,0 +1,16 @@
+package ie.gmit.labexam;
+
+public class Employee
+{
+    private String Title;
+    private String Name;
+    private String PPS;
+    private String Phone;
+    private String EmploymentType;
+    private Integer Age;
+
+    public Employee(String Title, String Name, String PPS, String Phone, String EmploymentType, Integer Age)
+    {
+
+    }
+}
---
+++ src/test/java/ie/gmit/labexam/EmployeeTest.java
@@ -0,0 +1,6 @@
+package ie.gmit.labexam;
+
+public class EmployeeTest
+{
+
+}
